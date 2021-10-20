d2883be07ebe36609c8f7ff0539a1d5cf908b43e
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-10-20 15:47:35 -0300

    instalado biblioteca responsavel pela interacao com o usuario perguntando o peso e altura, conforme especificado no exercicio 3 do dia 25.1

--- exercises/NodeJS_25.1/my-scripts/imc.js
+++ exercises/NodeJS_25.1/my-scripts/imc.js
@@ -1,3 +1,9 @@
+const readline = require('readline-sync');
+
+const name = readline.question('What is your name? ');
+const weight = readline.question('What is your weight? ');
+const height = readline.question('What is your height? ');
+
 const imc = (peso, altura) => (parseFloat(peso / (altura**2)).toPrecision(4))
 
 console.log(imc(75, 1.8))
