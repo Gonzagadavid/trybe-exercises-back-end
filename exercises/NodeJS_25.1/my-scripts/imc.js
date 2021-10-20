39625e074d29b994ac43437232debdfcb0d66972
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-10-20 16:04:09 -0300

    implementado para o usuario receber na tela a categoria do seu imc, conforme especificado no exercicio 5 do dia 25.1

--- exercises/NodeJS_25.1/my-scripts/imc.js
+++ exercises/NodeJS_25.1/my-scripts/imc.js
@@ -1,9 +1,17 @@
 const readline = require('readline-sync');
 
-const name = readline.question('What is your name? ');
-const weight = readline.questionInt('What is your weight? ');
-const height = readline.questionFloat('What is your height, in meters? ');
+const name = readline.question('Qual o seu nome?');
+const weight = readline.questionInt('qual seu peso?');
+const height = readline.questionFloat('qual sua altura? (em metros)');
 
-const imc = (peso, altura) => (parseFloat(peso / (altura**2)).toPrecision(4))
+const imc = (peso, altura) => {
+  const calc = parseFloat(peso / (altura**2)).toPrecision(4);
+  if (calc < 18.5) return `${calc}, Abaixo do peso (magreza)`;
+  if (calc < 25) return `${calc}, Peso normal`;
+  if (calc < 30) return `${calc}, Acima do peso (sobrepeso)`;
+  if (calc < 35) return `${calc}, Obesidade grau I`;
+  if (calc < 40) return `${calc}, Obesidade grau II`;
+  return `${calc}, Obesidade graus III e IV`;
+}
 
-console.log(imc(75, 1.8))
+console.log(`Olá ${name}! seu imc é de ${imc(weight, height)}`)
