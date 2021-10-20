bd426684b14ae851d4fe5dcde0d00572fe15a36c
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-10-20 15:52:04 -0300

    utilizado o metodo da biblioteca para receberem numeros flutuantes, conforme especificado no exercicio 4 do dia 25.1

--- exercises/NodeJS_25.1/my-scripts/imc.js
+++ exercises/NodeJS_25.1/my-scripts/imc.js
@@ -1,8 +1,8 @@
 const readline = require('readline-sync');
 
 const name = readline.question('What is your name? ');
-const weight = readline.question('What is your weight? ');
-const height = readline.question('What is your height? ');
+const weight = readline.questionInt('What is your weight? ');
+const height = readline.questionFloat('What is your height, in meters? ');
 
 const imc = (peso, altura) => (parseFloat(peso / (altura**2)).toPrecision(4))
 
