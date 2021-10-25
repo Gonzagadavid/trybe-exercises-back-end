9170e53efddf83c34686f5e42353dc2c33f18f0a
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-10-25 12:19:28 -0300

    adicionado rota para retornar a receita pelo id, e refatorado cliente para consumir esses dados

--- exercises-content/Express_25.4/client/main.js
+++ exercises-content/Express_25.4/client/main.js
@@ -1,4 +1,5 @@
 const content = document.getElementById('content');
+const nav = document.getElementById('nav')
 
 const getRecipes = async (url) => {
   const resp = await fetch(url);
@@ -7,9 +8,9 @@ const getRecipes = async (url) => {
   return recipes;
 }
 
-const createTable = (array, titleRecipe) => {
+const createTable = (array, type) => {
   const h2 = document.createElement('h2');
-  h2.innerHTML = titleRecipe;
+  h2.innerHTML = type;
   content.appendChild(h2)
   const menu = document.createElement('table');
   const thead = document.createElement('thead')
@@ -25,14 +26,27 @@ const createTable = (array, titleRecipe) => {
     th.innerHTML = title;
     tr.appendChild(th);
   })
-  
+
+  const addPedido = (id) => {
+    const list = JSON.parse(localStorage.getItem('express')) || { Meals: [], Drinks: []};
+    list[type].push(id)
+    localStorage.setItem('express', JSON.stringify(list))
+  }
+
   array.forEach((item) => {
     const trb = document.createElement('tr');
-     Object.values(item).forEach((element) => {
-       const td = document.createElement('td');
-       td.innerHTML = element
-       trb.appendChild(td)
+    const addBtn = document.createElement('button');
+    addBtn.innerHTML = 'ADD';
+    addBtn.addEventListener('click', () => addPedido(item.id));
+
+    Object.values(item).forEach((element) => {
+      const td = document.createElement('td');
+      td.innerHTML = element
+      trb.appendChild(td)
     })
+    const tdBtn = document.createElement('td');
+    tdBtn.appendChild(addBtn)
+    trb.appendChild(tdBtn)
     tbody.appendChild(trb)
   })
 }
@@ -43,5 +57,51 @@ const renderRecipes = async (type) => {
   createTable(recipe, type)
 }
 
-renderRecipes('Meals');
-renderRecipes('Drinks');
+const btnMenu = document.createElement('button');
+btnMenu.innerHTML = 'Menu'
+nav.appendChild(btnMenu)
+
+const renderMenu = () => {
+  content.innerHTML = ''
+  renderRecipes('Meals');
+  renderRecipes('Drinks');
+}
+
+btnMenu.addEventListener('click', renderMenu)
+
+const btnPedidos = document.createElement('button');
+btnPedidos.innerHTML = 'Pedidos'
+nav.appendChild(btnPedidos);
+
+const getPedidos = async() => {
+  content.innerHTML = ''
+  let total = 0
+  const { Meals, Drinks } = JSON.parse(localStorage.getItem('express')) || { Meals: [], Drinks: []};
+  const ul = document.createElement('ul');
+
+  Meals.forEach( async (id) => {
+    const recipe = await getRecipes(`http://localhost:3001/recipes/${id}`)
+    const { name, price } = recipe;
+    const li = document.createElement('li');
+    li.innerHTML = `${name}: ${price}`
+    ul.appendChild(li)
+    total += +price
+  })
+  
+  Drinks.forEach( async (id) => {
+    const recipe = await getRecipes(`http://localhost:3001/drinks/${id}`)
+    const { name, price } = recipe;
+    const li = document.createElement('li');
+    li.innerHTML = `${name}: ${price}`
+    ul.appendChild(li)
+    total += +price
+    totalPrice.innerHTML = `total$: ${total}`
+  })
+
+  content.appendChild(ul)
+  const totalPrice = document.createElement('p');
+  content.appendChild(totalPrice)
+  
+}
+
+btnPedidos.addEventListener('click', getPedidos);
--- exercises-content/Express_25.4/recipes/index.js
+++ exercises-content/Express_25.4/recipes/index.js
@@ -19,6 +19,7 @@ app.get('/recipes', function (req, res) {
 });
 
 // Crie uma array drinks com os seguintes obejtos dentro dela e uma rota GET /drinks que retorna a lista de bebidas.
+// Modifique tanto a rota de bebidas como de receitas para retornar a lista ordenada pelo nome em ordem alfabética.
 
 const drinks = [
   { id: 1, name: 'Refrigerante Lata', price: 5.0 },
@@ -38,7 +39,34 @@ app.get('/drinks', function (req, res) {
   res.json(drinks);
 });
 
-// Modifique tanto a rota de bebidas como de receitas para retornar a lista ordenada pelo nome em ordem alfabética.
+
+app.get('/recipes/:id', function (req, res) {
+  res.set({
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  });
+  const { id } = req.params;
+  const recipe = recipes.find((r) => r.id === parseInt(id));
+
+  if (!recipe) return res.status(404).json({ message: 'Recipe not found!'});
+
+  res.status(200).json(recipe);
+});
+
+// Crie uma rota GET /drink/:id para retornar uma bebida pelo id .
+
+app.get('/drinks/:id', function (req, res) {
+  res.set({
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  });
+  const { id } = req.params;
+  const drink = drinks.find((r) => r.id === parseInt(id));
+
+  if (!drink) return res.status(404).json({ message: 'Drink not found!'});
+
+  res.status(200).json(drink);
+});
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
