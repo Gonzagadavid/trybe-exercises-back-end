754d80dc428d561d85e07f2b3d61d7030e396546
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-10-25 21:39:10 -0300

    desenvolvido rota /hello de post e adicionado funcionalidade no cliente para testar a rota

--- exercises/Express_25.4/client/script.js
+++ exercises/Express_25.4/client/script.js
@@ -13,3 +13,27 @@ const getPing = async () => {
 }
 
 btnPing.addEventListener('click', getPing)
+
+const userName = document.getElementById('user-name');
+const hello = document.getElementById('hello');
+const btnHello = document.getElementById('btn-hello');
+
+const helloClick = async () => {
+  const data = JSON.stringify({ 'name' : `${userName.value}` });
+  const resp = await fetch('http://localhost:3500/hello/', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'text/plain'// <----exerc2 foi necessario mudar de  application/json para text/plain
+    },
+    body: data,
+    })
+  if (!resp.ok) console.log('ocorreu um erro com hello')
+ const { message } = await resp.json();
+
+ hello.innerHTML = message;
+
+ setTimeout(() => { hello.innerHTML = ''}, 2000)
+}
+
+btnHello.addEventListener('click', helloClick)
--- exercises/Express_25.4/server/index.js
+++ exercises/Express_25.4/server/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express()
-
+app.use(express.json({ type: ['application/json', 'text/plain'] }))// <---exerc 2 foi necessario adicionar os parametros 
+// pois o body estava chegando vazio, outra forma valida é adicionar o bodyParser(descontinuado)
 
 app.get('/ping', (req, res) => {
   res.set({
@@ -11,6 +12,16 @@ app.get('/ping', (req, res) => {
   });
 
   res.json({ message: 'pong' })
+})
+
+app.post('/hello', (req, res) => {
+  res.set({
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  });
+  const { name } = req.body;
+  
+  res.status(201).json({ message: `Hello, ${name}!!!`});
 })
 
 app.listen(3500, () => { console.log('servidor rodando na porta 3500')});
