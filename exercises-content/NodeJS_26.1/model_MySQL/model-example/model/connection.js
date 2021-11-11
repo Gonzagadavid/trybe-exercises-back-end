033ac6505e32e719c4f85f27fae775deb1968cb2
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-11-11 15:16:23 -0300

    adiconado primeira parte do conteudo

---
+++ exercises-content/NodeJS_26.1/model_MySQL/model-example/model/connection.js
@@ -0,0 +1,11 @@
+// models/connection.js
+
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'david',
+    password: '1234',
+    database: 'model_example' });
+
+module.exports = connection;
