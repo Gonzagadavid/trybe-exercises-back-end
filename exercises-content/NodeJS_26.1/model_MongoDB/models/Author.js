6e0694b97024c496e5cf717a0ea9198475ebb955
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-11-11 21:08:56 -0300

    adicionado segunda parte da segunda parte do conteudo com mongo e feito exercicios de fixação

--- exercises-content/NodeJS_26.1/model_MongoDB/models/Author.js
+++ exercises-content/NodeJS_26.1/model_MongoDB/models/Author.js
@@ -1,5 +1,6 @@
 // models/Author.js
 
+const { ObjectId } = require('mongodb');
 const connection = require('./connection');
 
 // Cria uma string com o nome completo do autor
@@ -51,22 +52,18 @@ Busca um autor específico, a partir do seu ID
 */
 
 const findById = async (id) => {
-    // Repare que substituímos o id por `?` na query.
-    // Depois, ao executá-la, informamos um array com o id para o método `execute`.
-    // O `mysql2` vai realizar, de forma segura, a substituição do `?` pelo id informado.
-    const query = 'SELECT id, first_name, middle_name, last_name FROM model_example.authors WHERE id = ?'
-    const [ authorData ] = await connection.execute(query, [id]);
-    
-    if (authorData.length === 0) return null;
-    
-    // Utilizamos [0] para buscar a primeira linha, que deve ser a única no array de resultados, pois estamos buscando por ID.
-    const { firstName, middleName, lastName } = serialize(authorData[0]);
-    
-    return getNewAuthor({
-        id,
-        firstName,
-        middleName,
-        lastName});
+    if (!ObjectId.isValid(id)) {
+        return null;
+    }
+
+    const authorData = await connection()
+        .then((db) => db.collection('authors').findOne(new ObjectId(id)));
+
+    if (!authorData) return null;
+
+    const { firstName, middleName, lastName } = authorData;
+
+    return getNewAuthor({ id, firstName, middleName, lastName });
 };
     
 const isValid = (firstName, middleName, lastName) => {
@@ -77,10 +74,11 @@ const isValid = (firstName, middleName, lastName) => {
     return true;
 };
 
-const create = async (firstName, middleName, lastName) => connection.execute(
-    'INSERT INTO model_example.authors (first_name, middle_name, last_name) VALUES (?,?,?)',
-    [firstName, middleName, lastName],
-);
+const create = async (firstName, middleName, lastName) =>
+    connection()
+        .then((db) => db.collection('authors').insertOne({ firstName, middleName, lastName }))
+        .then(result => getNewAuthor({ id: result.insertedId, firstName, middleName, lastName }));
+
 
 module.exports = {
   getAll,
