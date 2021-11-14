8ffb30655981828c7c879dd17d10c1172543c71b
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-11-14 16:02:00 -0300

    adicionado verificação de formato com joi regex, no middleware validFields

--- exercises/NodeJS_26.2/cep-lookup/middleware/validFields.js
+++ exercises/NodeJS_26.2/cep-lookup/middleware/validFields.js
@@ -5,7 +5,9 @@ const getMessage = ({ details: [{ message }] }) => message;
 
 const validFields = (req, res, next) => {
   const { error } = Joi.object({
-    cep: Joi.string().not().empty().required(),
+    cep: Joi.string().not().empty().regex(/^\d{5}-\d{3}$/)
+      .messages({ 'string.pattern.base': 'cep format invalid' })
+      .required(),
     logradouro: Joi.string().not().empty().required(),
     bairro: Joi.string().not().empty().required(),
     localidade: Joi.string().not().empty().required(),
